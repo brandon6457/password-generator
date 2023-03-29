7d2e8f7b1f64db46f49c4b6537e8b45a439a5031
brandon6457 <brandon6457@gmail.com>
2023-03-29 17:40:39 -0400

    added some js content

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // Character types
+  var lowercase = confirm("Include lowercase letters?");
+  var uppercase = confirm("Include uppercase letters?");
+  var numeric = confirm("Include numeric characters?");
+  var special = confirm("Include special characters?");
+  if (!lowercase &&!uppercase &&!numeric &&!special) {
+    alert("You must choose at least one character type.");
+    return;
 
-}
-
+    passwordText.value = password;
+  }
+  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
