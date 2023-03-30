e1635125835d076fae79c01d5631fe3c39695031
brandon6457 <brandon6457@gmail.com>
2023-03-30 15:06:20 -0400

    made changes to js file

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var passwordLength = parseInt
-  prompt("Choose a number between 8 and 128");
+  var passwordLength = parseInt(
+  prompt("Choose a number between 8 and 128"));
 
-  while (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = parseInt
-    prompt("Please choose a number between 8 and 128"); 
+    if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please choose a number between 8 and 128"); 
+    return;
   }
   
   // Character types
@@ -16,40 +16,41 @@ function generatePassword() {
   var numeric = confirm("Include numeric characters?");
   var special = confirm("Include special characters?");
 
-  while (!lowercase &&!uppercase &&!numeric &&!special) {
+    if (!lowercase &&!uppercase &&!numeric &&!special) {
     alert("You must choose at least one character type.");
+    return;
   }
     var password = "";
-    var possibleCharacters = "";
+    //var possibleCharacters = "";
     var characterTypes = "";
 
     if (lowercase) {
-      possibleCharacters += "abcdefghijklmnopqrstuvwxyz";
-      characterTypes += "lowercase";
+      characterTypes += "abcdefghijklmnopqrstuvwxyz";
+       //possibleCharacters += "lowercase";
     }
     if (uppercase) {
-      possibleCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      characterTypes += "uppercase";
+      characterTypes += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      //possibleCharacters += "uppercase";
     }
     if (numeric) {
-      possibleCharacters += "0123456789";
-      characterTypes += "numeric";
+      characterTypes += "0123456789";
+      //possibleCharacters += "numeric";
     }
     if (special) {
-      possibleCharacters += "!@#$%^&*()_+";
-      characterTypes += "special";
+      characterTypes += "!@#$%^&*()_+";
+      //possibleCharacters += "special";
     }
     alert("You have selected: " + characterTypes + ".");
 
     for (var i = 0; i < passwordLength; i++) {
-      password += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+      password += characterTypes[Math.floor(Math.random() * characterTypes.length)];
     }
       console.log(password);
       return password;
 
 }
   function writePassword() {
-    var passwordLength = generatePassword(); 
+    var password= generatePassword(); 
     var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
